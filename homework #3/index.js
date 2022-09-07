b9136d8574cd1b93d959c9a5e916bbd1fcd75524
Nikita Kuznetsov <kuznetsovfrontend@gmail.com>
2022-09-07 11:29:43 +0200

    added js file

---
+++ homework #3/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+let a = prompt("FirstNum");
+let b = prompt("SecondNum");
+let sum = a + b;
+
+// if (sum > 20) { // 1 example
+//   alert(":))");
+// } else {
+//   alert(":((");
+// }
+
+sum > 20 ? alert(":))") : alert(":(("); // 2 example
